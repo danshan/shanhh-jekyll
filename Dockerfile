2db53146c6207dc3b6ff9fe59ea77b9db45ee189
dan <i@shanhh.com>
2017-03-03 16:40:30 +0800

    feature remove nginx

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,9 @@ MAINTAINER i@shanhh.com
 ADD jekyll /opt/data/jekyll
 ADD qiniu /opt/data/qiniu
 
-WORKDIR /opt/data/jekyll
-RUN jekyll build
-
-RUN rm -rf /etc/nginx/sites-enabled/default
-ADD nginx/shanhh.com /etc/nginx/sites-available/shanhh.com
-RUN ln -s /etc/nginx/sites-available/shanhh.com /etc/nginx/sites-enabled/shanhh.com
-
 ADD bin/entrypoint.sh /opt/data/entrypoint.sh
+
+WORKDIR /opt/data/jekyll
 
 EXPOSE 80
 ENTRYPOINT ["/opt/data/entrypoint.sh"]
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-service nginx start
-
-# /jekyll/tools/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
-
 sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /opt/data/qiniu/qupload_*.json
 sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /opt/data/qiniu/qupload_*.json
 /opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_assets.json
 /opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_files.json
 
-while :; do
-  sleep 3000
-done
+jekyll serve
+
