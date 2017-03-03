c153764ba965dea16b49767f9c53c674af2ddf71
dan <i@shanhh.com>
2017-03-03 17:26:22 +0800

    feature listen 0.0.0.0

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,5 +5,5 @@ sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /opt/data/qiniu/qupload_*.js
 /opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_assets.json
 /opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_files.json
 
-jekyll serve
+jekyll serve --host=0.0.0.0
 
