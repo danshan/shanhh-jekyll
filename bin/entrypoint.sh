d61df8eccf446a6fee974144b96f1420cc44ab22
dan <i@shanhh.com>
2017-03-03 08:58:08 +0800

    feature update qiniu path

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,10 +4,10 @@ service nginx start
 
 # /jekyll/tools/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
 
-sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /jekyll/qiniu/qupload_*.json
-sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /jekyll/qiniu/qupload_*.json
-/jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
-/jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
+sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /opt/data/qiniu/qupload_*.json
+sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /opt/data/qiniu/qupload_*.json
+/opt/data/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_assets.json
+/opt/data/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_files.json
 
 while :; do
   sleep 3000
