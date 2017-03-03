6f67f1291fbaec26ad7ac3c87d6a3848590b6ee5
dan <i@shanhh.com>
2017-03-03 09:17:30 +0800

    update qiniu path

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,8 +6,8 @@ service nginx start
 
 sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /opt/data/qiniu/qupload_*.json
 sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /opt/data/qiniu/qupload_*.json
-/opt/data/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_assets.json
-/opt/data/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_files.json
+/opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_assets.json
+/opt/tools/qshell/qshell_linux_amd64 qupload /opt/data/qiniu/qupload_files.json
 
 while :; do
   sleep 3000
