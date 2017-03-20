5e24ac56d46de6ad86ee1b3d56858f2a67c9fb77
dan <i@shanhh.com>
2017-03-20 10:22:10 +0800

    feature update image

--- jekyll/_plugins/img_tag.rb
+++ jekyll/_plugins/img_tag.rb
@@ -1,14 +1,16 @@
 module Jekyll
   class RenderImageTag < Liquid::Tag
-      attr_accessor :site
-
     def initialize(tag_name, text, tokens)
       super
       @text = text
     end
 
     def render(context)
-      "<figure><a href=\"#{site.cdn}#{@text}#{site.img_origin}\"><img src=\"#{site.cdn}#{@text}#{site.img}\"></a></figure>"
+      cdn = context.registers[:site].config["cdn"]
+      img = context.registers[:site].config["img"]
+      img_origin = context.registers[:site].config["img_origin"]
+
+      "<figure><a href=\"#{cdn}#{@text}#{img_origin}\"><img src=\"#{cdn}#{@text}#{img}\"></a></figure>"
     end
   end
 end
