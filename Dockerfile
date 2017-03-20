5f3bf3b3e20f77ac4b25751d329d8ff98f5a6e93
dan <i@shanhh.com>
2017-03-20 08:47:10 +0800

    feature 后台上传图片

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER i@shanhh.com
 
 ADD jekyll /opt/data/jekyll
 ADD qiniu /opt/data/qiniu
+RUN mkdir -p /var/log/qiniu
 
 ADD bin/entrypoint.sh /opt/data/entrypoint.sh
 
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,8 +2,8 @@
 
 sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /opt/data/qiniu/qupload_*.json
 sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /opt/data/qiniu/qupload_*.json
-/opt/tools/qshell/qshell_linux_amd64 qupload 20 /opt/data/qiniu/qupload_assets.json
-/opt/tools/qshell/qshell_linux_amd64 qupload 20 /opt/data/qiniu/qupload_files.json
+/opt/tools/qshell/qshell_linux_amd64 qupload 20 /opt/data/qiniu/qupload_assets.json >> /var/log/qiniu/qupload.log &
+/opt/tools/qshell/qshell_linux_amd64 qupload 20 /opt/data/qiniu/qupload_files.json >> /var/log/qiniu/qupload.log &
 
 JEKYLL_ENV=production jekyll serve --host=0.0.0.0
 
